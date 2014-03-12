aefa00237b6eb8948043d25ec972f7bea1b75156
pulicapus <pulicapus@yandex.ua>
2014-03-12 11:05:17 +0200

    update module

--- tmpl/default.php
+++ tmpl/default.php
@@ -5,66 +5,61 @@
 	JHtml::addIncludePath(JPATH_BASE.'/components/com_content/helpers');
 }
 ?>
-
 <div class="mod-article-single mod-article-single__<?php echo $moduleclass_sfx; ?>" id="module_<?php echo $module->id; ?>">
 	<div class="item__module" id="item_<?php echo $item->id; ?>">
-
-	<!-- Icons -->
-<?php if($layout!='edit') : ?>
-	<?php if ($canEdit) : ?>
-	<div class="item_icons btn-group pull-right"> <a class="btn dropdown-toggle" data-toggle="dropdown" href="#"> <i class="fa fa-cog"></i> <span class="caret"></span> </a>
-		<ul class="dropdown-menu">
-			<li class="edit-icon"> <?php echo JHtml::_('icon.edit', $item, $params); ?> </li>
-		</ul>
-	</div>
-	<?php endif; ?>
-	<?php endif; ?>
-
+		<?php if($layout!='edit') :
+		if ($canEdit) : ?>
+		<!-- Icons -->
+		<div class="item_icons btn-group pull-right">
+			<a class="btn dropdown-toggle" data-toggle="dropdown" href="#"> <i class="fa fa-cog"></i> <span class="caret"></span> </a>
+			<ul class="dropdown-menu">
+				<li class="edit-icon"> <?php echo JHtml::_('icon.edit', $item, $params); ?> </li>
+			</ul>
+		</div>
+		<?php endif;
+		endif;
+		if ($params->get('item_title')) : ?>
 		<!-- Item Title -->
-		<?php if ($params->get('item_title')) : ?>
-			<<?php echo $item_heading; ?> class="item-title">
-				<?php if ($params->get('link_titles') && isset($item->link)) : ?>
-					<a href="<?php echo $item->link;?>">
-						<?php echo $item->title;?></a>
-				<?php else : ?>
-					<?php echo $item->title; ?>
-				<?php endif; ?>
-			</<?php echo $item_heading; ?>>
-		<?php endif; ?>
-		<?php echo $item->afterDisplayTitle; ?>
-		<?php echo $item->beforeDisplayContent; ?>
+		<<?php echo $item_heading; ?> class="item-title">
+			<?php if ($params->get('link_titles') && isset($item->link)) : ?>
+			<a href="<?php echo $item->link;?>"><?php echo $item->title;?></a>
+			<?php else :
+			echo $item->title;
+			endif; ?>
+		</<?php echo $item_heading; ?>>
+		<?php endif;
+		echo $item->afterDisplayTitle;
+		echo $item->beforeDisplayContent;
 
+		if ($params->get('published_on')) : ?>
 		<!-- Publish Date -->
-		<?php if ($params->get('published_on')) : ?>
-			<span class="item_published">
-				<i class="icon-calendar"></i><?php echo JText::sprintf(JHtml::_('date', $item->publish_up, JText::_('DATE_FORMAT_LC3'))); ?>
-			</span>
-		<?php endif; ?>
+		<time datetime="<?php echo JHtml::_('date', $item->publish_up, 'Y-m-d H:i'); ?>" class="item_published">
+			<?php echo JText::sprintf(JHtml::_('date', $item->publish_up, JText::_('DATE_FORMAT_LC3'))); ?>
+		</time>
+		<?php endif;
 
+		if ($params->get('show_intro_image')) :
+		if (isset($item_images->image_intro) and !empty($item_images->image_intro)) :
+		$imgfloat = (empty($item_images->float_intro)) ? $params->get('float_intro') : $item_images->float_intro; ?>
 		<!-- Intro Image -->
-		<?php if ($params->get('show_intro_image')) : ?>
-			<?php  if (isset($item_images->image_intro) and !empty($item_images->image_intro)) : ?>
-			<?php $imgfloat = (empty($item_images->float_intro)) ? $params->get('float_intro') : $item_images->float_intro; ?>
-			<div class="item_img img-intro img-intro__<?php echo htmlspecialchars($imgfloat); ?>">
+		<figure class="item_img img-intro img-intro__<?php echo htmlspecialchars($imgfloat); ?>">
 			<?php if ((($params->get('item_title') && $params->get('link_titles')) || $params->get('readmore')) && isset($item->link)) : ?>
 			<a href="<?php echo $item->link;?>">
 			<?php endif; ?>
-			<img
-				<?php if ($item_images->image_intro_caption):
-					echo 'class="caption"'.' title="' .htmlspecialchars($item_images->image_intro_caption) .'"';
-				endif; ?>
-				src="<?php echo htmlspecialchars($item_images->image_intro); ?>" alt="<?php echo htmlspecialchars($item_images->image_intro_alt); ?>"/>
-				<?php if ((($params->get('item_title') && $params->get('link_titles')) || $params->get('readmore')) && isset($item->link)) : ?>
-				</a>
-				<?php endif; ?>
-				</div>
+				<img src="<?php echo htmlspecialchars($item_images->image_intro); ?>" alt="<?php echo htmlspecialchars($item_images->image_intro_alt); ?>">
+				<?php if ($item_images->image_intro_caption): ?>
+				<figcaption><?php echo htmlspecialchars($item_images->image_intro_caption); ?></figcaption>
+				<?php endif;
+			if ((($params->get('item_title') && $params->get('link_titles')) || $params->get('readmore')) && isset($item->link)) : ?>
+			</a>
 			<?php endif; ?>
-		<?php endif; ?>
+		</figure>
+		<?php endif;
+		endif; ?>
 
 		<!-- Intro Text -->
 		<div class="item_introtext">
-			<?php echo $item->introtext; ?>
-			<?php 
+			<?php echo $item->introtext;
 				if (isset($item->link) && $params->get('readmore')) :	
 
             if($view == "form"){
@@ -84,27 +79,25 @@
 				endif; ?>
 		</div>	
 	</div>
-
   <?php if($params->get('mod_button') == 1): ?>   
-    <div class="mod-newsflash-adv_custom-link">
-      <?php 
-        $menuLink = $menu->getItem($params->get('custom_link_menu'));
+  <div class="mod-newsflash-adv_custom-link">
+    <?php 
+      $menuLink = $menu->getItem($params->get('custom_link_menu'));
 
-          switch ($params->get('custom_link_route')) 
-          {
-            case 0:
-              $link_url = $params->get('custom_link_url');
-              break;
-            case 1:
-              $link_url = JRoute::_($menuLink->link.'&Itemid='.$menuLink->id);
-              break;            
-            default:
-              $link_url = "#";
-              break;
-          }
-          echo '<a href="'. $link_url .'">'. $params->get('custom_link_title') .'</a>';
-      ?>
-    </div>
+        switch ($params->get('custom_link_route')) 
+        {
+          case 0:
+            $link_url = $params->get('custom_link_url');
+            break;
+          case 1:
+            $link_url = JRoute::_($menuLink->link.'&Itemid='.$menuLink->id);
+            break;            
+          default:
+            $link_url = "#";
+            break;
+        }
+        echo '<a href="'. $link_url .'">'. $params->get('custom_link_title') .'</a>';
+    ?>
+  </div>
   <?php endif; ?>
-
 </div>
